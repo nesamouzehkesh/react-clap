6060b842aeba853c0b401ff458d651c09e4d3b39
nesamouzehkesh <n.mouzehkesh@gmail.com>
2018-05-03 16:41:02 +1000

    css added for CardSHow

--- src/components/CardShow/CardShow.js
+++ src/components/CardShow/CardShow.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './CardShow.css';
 
 const CardShow = ({ show }) => (
-    <li>
-        {show.name}
-        {show.image}
-        {show.url}
+    <li className="Card">
+        <img src={show.image} alt="" className="author-image" />
+        <h3>{show.name}</h3>
+        <a href={show.url} className="author-medium">Go to article on Medium</a>
     </li>
 )
 
