1988ec7ce2b05a5f2742bc4cc87d64acad8c6917
nesamouzehkesh <n.mouzehkesh@gmail.com>
2018-05-03 15:24:52 +1000

    main html and css and mounting point added

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+React.Dom.render(<App />, document.getElementById('root'));
+
