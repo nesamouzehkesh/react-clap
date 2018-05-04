810d075456455b3b787120fda7a2ba3d99e2db79
nesamouzehkesh <n.mouzehkesh@gmail.com>
2018-05-04 14:28:03 +1000

    more styling and UX fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './Header.css';
 import '../../icons.css';
 
-const Header = ({ title }) => (
+const Header = ({ title, add }) => (
     <div className="header-container">
         <div className="fi-paw">
             <p>{title}</p>
         </div>
         <input className="search-input" />
+        <div className="fi-plus">{` ${add}`}</div>
     </div>
 )
 
