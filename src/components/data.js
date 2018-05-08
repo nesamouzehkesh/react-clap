e7022b7ab4bc459a4786b6901dc068e612b651c2
nesamouzehkesh <n.mouzehkesh@gmail.com>
2018-05-08 21:16:20 +1000

    images update

--- src/components/data.js
+++ src/components/data.js
@@ -4,15 +4,22 @@ export default [
     id: '1',
     image: './assets/img/async.png',
     url: 'https://open.spotify.com/artist/1QOmebWGB6FdFtW7Bo3F0W',
-    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sollicitudin elementum mollis. Aliquam aliquam elit vel augue viverra elementum. Vivamus turpis est, euismod sed nulla sed, pulvinar finibus metus. Duis quis metus sed ante porta tempor. Aliquam ac sollicitudin nisl, at rhoncus magna. Aenean imperdiet lectus sodales, maximus urna sed, egestas ipsum. Vivamus vehicula in urna in ornare. Aliquam pharetra leo in diam aliquam ullamcorper. Fusce nec placerat odio. Phasellus convallis ultrices ultricies. Donec vitae venenatis mi. Nullam bibendum eros a felis faucibus luctus. Praesent in lobortis elit."
+    summary: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Nam sollicitudin elementum mollis. Aliquam aliquam elit vel augue viverra elementum. 
+    Vivamus turpis est, euismod sed nulla sed, pulvinar finibus metus. Duis quis metus sed 
+    ante porta tempor. Aliquam ac sollicitudin nisl, at rhoncus magna. Aenean imperdiet lectus 
+    sodales, maximus urna sed, egestas ipsum. Vivamus vehicula in urna in ornare. Aliquam 
+    pharetra leo in diam aliquam ullamcorper. Fusce nec placerat odio. Phasellus convallis 
+    ultrices ultricies. Donec vitae venenatis mi. Nullam bibendum eros a felis faucibus luctus. 
+    Praesent in lobortis elit.`
   },
   { name: 'CSS', id: '2', image: './assets/img/css.png', url: 'https://open.spotify.com/artist/1MuQ2m2tg7naeRGAOxYZer' },
   { name: 'ES6', id: '3', image: './assets/img/es6.png', url: 'https://open.spotify.com/artist/53XhwfbYqKCa1cC15pYq2q' },
   { name: 'Functional Programming', id: '4', image: './assets/img/functional.jpg', url: 'https://open.spotify.com/artist/0k17h0D3J5VfsdmQ1iZtE9' },
-  { name: 'LESS', id: '5', image: './assets/img/less.jpg', url: 'https://open.spotify.com/artist/3qm84nBOXUEQ2vnTfUTTFC' },
-  { name: 'MotionJs', id: '6', image: './assets/img/motion.jpg', url: 'https://open.spotify.com/artist/4gzpq5DPGxSnKTe4SA8HAU' },
-  { name: 'NodeJs', id: '7', image: './assets/img/nodejs.jpg', url: 'https://open.spotify.com/artist/1dfeR4HaWDbWqFHLkxsg1d' },
+  { name: 'LESS', id: '5', image: './assets/img/less.png', url: 'https://open.spotify.com/artist/3qm84nBOXUEQ2vnTfUTTFC' },
+  { name: 'MotionJs', id: '6', image: './assets/img/motion.png', url: 'https://open.spotify.com/artist/4gzpq5DPGxSnKTe4SA8HAU' },
+  { name: 'NodeJs', id: '7', image: './assets/img/nodejs.png', url: 'https://open.spotify.com/artist/1dfeR4HaWDbWqFHLkxsg1d' },
   { name: 'React Todo', id: '8', image: './assets/img/react.jpg', url: 'https://open.spotify.com/artist/7oPftvlwr6VrsViSDV7fJY' },
-  { name: 'VueJs', id: '9', image: './assets/img/vue.jpg', url: 'https://open.spotify.com/artist/7Ln80lUS6He07XvHI8qqHH' },
+  { name: 'VueJs', id: '9', image: './assets/img/vue.png', url: 'https://open.spotify.com/artist/7Ln80lUS6He07XvHI8qqHH' },
   { name: 'Javascript Closures', id: '10', image: './assets/img/closure.png', url: 'https://open.spotify.com/artist/3WrFJ7ztbogyGnTHbHJFl2' }
 ];
