ab7679418661ea3178845d68e07ee306b4bcdf5d
nesamouzehkesh <n.mouzehkesh@gmail.com>
2018-05-09 14:31:17 +1000

    style bug fixed

--- src/components/data.js
+++ src/components/data.js
@@ -15,11 +15,11 @@ export default [
   },
   { name: 'CSS', id: '2', image: './assets/img/css.png', url: 'https://open.spotify.com/artist/1MuQ2m2tg7naeRGAOxYZer' },
   { name: 'ES6', id: '3', image: './assets/img/es6.png', url: 'https://open.spotify.com/artist/53XhwfbYqKCa1cC15pYq2q' },
-  { name: 'Functional Programming', id: '4', image: './assets/img/functional.jpg', url: 'https://open.spotify.com/artist/0k17h0D3J5VfsdmQ1iZtE9' },
+  { name: 'Functional Programming', id: '4', image: './assets/img/functional.png', url: 'https://open.spotify.com/artist/0k17h0D3J5VfsdmQ1iZtE9' },
   { name: 'LESS', id: '5', image: './assets/img/less.png', url: 'https://open.spotify.com/artist/3qm84nBOXUEQ2vnTfUTTFC' },
   { name: 'MotionJs', id: '6', image: './assets/img/motion.png', url: 'https://open.spotify.com/artist/4gzpq5DPGxSnKTe4SA8HAU' },
   { name: 'NodeJs', id: '7', image: './assets/img/nodejs.png', url: 'https://open.spotify.com/artist/1dfeR4HaWDbWqFHLkxsg1d' },
-  { name: 'React Todo', id: '8', image: './assets/img/react.jpg', url: 'https://open.spotify.com/artist/7oPftvlwr6VrsViSDV7fJY' },
+  { name: 'React Todo', id: '8', image: './assets/img/react.png', url: 'https://open.spotify.com/artist/7oPftvlwr6VrsViSDV7fJY' },
   { name: 'VueJs', id: '9', image: './assets/img/vue.png', url: 'https://open.spotify.com/artist/7Ln80lUS6He07XvHI8qqHH' },
   { name: 'Javascript Closures', id: '10', image: './assets/img/closure.png', url: 'https://open.spotify.com/artist/3WrFJ7ztbogyGnTHbHJFl2' }
 ];
