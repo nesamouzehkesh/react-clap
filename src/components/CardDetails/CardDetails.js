e129c3bf3977fce732fe18d31c5a2641fad1ee6e
nesamouzehkesh <n.mouzehkesh@gmail.com>
2018-05-09 13:42:43 +1000

    css for clap converted from sass

--- src/components/CardDetails/CardDetails.js
+++ src/components/CardDetails/CardDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './CardDetails.css';
 import '../../icons.css';
+import Clap from '../Clap/Clap';
 
 const CardDetails = ({ currentArticle, renderMainList }) => (
     <div>
@@ -8,16 +9,17 @@ const CardDetails = ({ currentArticle, renderMainList }) => (
             <span className="fi-zoom-in">
                 <span><b>Article Name: </b></span>
             </span>
-            <p>{currentArticle.name}</p>
+            <p className="paragraph">{currentArticle.name}</p>
 
             <span className="fi-annotate">
                 <span><b>Article Summary: </b></span>
             </span>
-            <p>{currentArticle.summary}</p>
+            <p className="paragraph">{currentArticle.summary}</p>
 
             <span className="fi-anchor"><a className="backlink" href={currentArticle.url}>
                 Take me to the original posting
             </a></span>
+            <Clap />
 
             <ul className="footer">
                 <li><a href="#" className="icon"><img src={require('./paw2.png')} alt="paw" /></a></li>
---
+++ src/components/Clap/Clap.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Clap.css';
+
+const Clap = () => (
+    <p>I am gonna be clap</p>
+)
+
+export default Clap;
